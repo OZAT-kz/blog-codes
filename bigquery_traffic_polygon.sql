451973090ba2d95c2b335f0392d864dacffb6590
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-12 13:34:19 +0500

    Auto publish: bigquery_traffic_polygon.sql

---
+++ bigquery_traffic_polygon.sql
@@ -0,0 +1,14 @@
+// ==============================================================================
+// BigQuery GIS Traffic Polygon
+// Source: OZAT Engineering Blog (https://ozat.kz)
+// GitHub: https://github.com/OZAT-kz/blog-codes/blob/main/bigquery_traffic_polygon.sql
+// ==============================================================================
+
+
+-- Кептеліс полигонын құру SQL сұрауы
+SELECT
+  ST_CONVEXHULL(ST_UNION_AGG(geo_point)) as traffic_jam_polygon,
+  COUNT(DISTINCT courier_id) as stuck_couriers_count
+FROM clustered_data
+WHERE cluster_id IS NOT NULL
+GROUP BY cluster_id;
