93abf1e7fa09b71ea90de89e55188493dea5e44c
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-12 13:17:03 +0500

    Auto publish: dataflow_gps_clean.py

---
+++ dataflow_gps_clean.py
@@ -0,0 +1,26 @@
+# ==============================================================================
+# Dataflow (Apache Beam) GPS Cleaning
+# Source: OZAT Engineering Blog (https://ozat.kz)
+# GitHub: https://github.com/OZAT-kz/blog-codes/blob/main/dataflow_gps_clean.py
+# ==============================================================================
+
+
+# Фрагмент пайплайна Dataflow (Apache Beam)
+import apache_beam as beam
+import json
+
+def parse_and_clean_gps(message):
+    data = json.loads(message)
+    # Фильтруем аномалии GPS (например, отстрелы в Капчагай)
+    if 43.0 < data['lat'] < 43.6 and 76.6 < data['lon'] < 77.2:
+        yield data
+
+(p 
+ | 'ReadFromPubSub' >> beam.io.ReadFromPubSub(subscription='projects/ozat-kz/subscriptions/gps-sub')
+ | 'CleanData' >> beam.FlatMap(parse_and_clean_gps)
+ | 'WriteToBigQuery' >> beam.io.WriteToBigQuery(
+       table='ozat-kz:traffic_dataset.raw_gps',
+       create_disposition=beam.io.BigQueryDisposition.CREATE_NEVER,
+       write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND
+   )
+)
