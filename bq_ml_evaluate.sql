5abea8a96198f8fec4fe5dc9ffbd7bf0c71b9adf
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-12 14:15:56 +0500

    Auto publish: bq_ml_evaluate.sql

---
+++ bq_ml_evaluate.sql
@@ -0,0 +1,7 @@
+// ==============================================================================
+// BigQuery ML Model Evaluation
+// Source: OZAT Engineering Blog (https://ozat.kz)
+// GitHub: https://github.com/OZAT-kz/blog-codes/blob/main/bq_ml_evaluate.sql
+// ==============================================================================
+
+[sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS][sS]
