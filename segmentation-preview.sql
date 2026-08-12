989caa1612736db7ede69cfeddc4dd2adab03d69
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-12 11:58:30 +0500

    Add/Update: segmentation-preview.sql

---
+++ segmentation-preview.sql
@@ -0,0 +1,12 @@
+// ==============================================================================
+// User propensity score segmentation preview
+// Source: OZAT Engineering Blog (https://ozat.kz)
+// GitHub: https://github.com/OZAT-kz/blog-codes/blob/main/segmentation-preview.sql
+// ==============================================================================
+
+
+user_id | propensity_score | segment
+------------------------------------
+user_A  | 0.89             | High
+user_B  | 0.05             | Low
+user_C  | 0.45             | Medium
