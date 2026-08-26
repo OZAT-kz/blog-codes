ac5ea547e54505aefdf7caa4f814e83ffe7526d8
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-26 14:00:31 +0500

    Update code snippet: cloud-armor-bot-rule.js

--- cloud-armor-bot-rule.js
+++ cloud-armor-bot-rule.js
@@ -4,6 +4,6 @@
 // GitHub: https://github.com/OZAT-kz/blog-codes/blob/main/cloud-armor-bot-rule.js
 // ==============================================================================
 
-# Пример кастомного правила Cloud Armor на CEL
+# CEL тіліндегі Cloud Armor кастомды ережесінің мысалы
 request.headers['user-agent'].contains('HeadlessChrome') || 
 (request.headers['accept-language'] == 'en-US,en;q=0.5' && request.path.matches('/api/v1/search'))
