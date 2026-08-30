2694fde8943147cf16a6ccbd17c4a1ac077ca280
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:51:26 +0500

    Update header to OZAT Engineering Hub: cloud-armor-bot-rule.js

--- cloud-armor-bot-rule.js
+++ cloud-armor-bot-rule.js
@@ -4,6 +4,5 @@
 // GitHub: https://github.com/OZAT-kz/blog-codes/blob/main/cloud-armor-bot-rule.js
 // ==============================================================================
 
-# Пример кастомного правила Cloud Armor на CEL
 request.headers['user-agent'].contains('HeadlessChrome') || 
 (request.headers['accept-language'] == 'en-US,en;q=0.5' && request.path.matches('/api/v1/search'))
