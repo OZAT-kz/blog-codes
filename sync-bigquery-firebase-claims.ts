6ad04db3f879ae7ce4202d7589654148685bca2d
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:35:55 +0500

    Update header to OZAT Engineering Hub: sync-bigquery-firebase-claims.ts

--- sync-bigquery-firebase-claims.ts
+++ sync-bigquery-firebase-claims.ts
@@ -1,6 +1,6 @@
 // ==============================================================================
 // sync-bigquery-firebase-claims.ts
-// Source: OZAT Engineering Blog (https://ozat.kz)
+// Source: OZAT Engineering Hub (https://ozat.kz)
 // GitHub: https://github.com/OZAT-kz/blog-codes/blob/main/sync-bigquery-firebase-claims.ts
 // ==============================================================================
 
