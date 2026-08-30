b5f5f8eef79f79d3d8cc4e9efa2c1b059a4744f8
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:51:39 +0500

    Update header to OZAT Engineering Hub: dbt-staging-ga4.sql

--- dbt-staging-ga4.sql
+++ dbt-staging-ga4.sql
@@ -4,14 +4,6 @@
 -- GitHub: https://github.com/OZAT-kz/blog-codes/blob/main/dbt-staging-ga4.sql
 -- ==============================================================================
 
--- Пример dbt-модели для извлечения параметров GA4 (Staging)
-SELECT
-  event_date,
-  event_timestamp,
-  event_name,
-  user_pseudo_id AS client_id,
-  (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'campaign') AS utm_campaign,
-  (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'source') AS utm_source
 FROM
   {{ source('google_analytics', 'events') }}
 WHERE
