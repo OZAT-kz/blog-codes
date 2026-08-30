3b2a73ef41ad4e1020663da7643f4092739ec30c
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:34:29 +0500

    Update header to OZAT Engineering Hub: bq_ml_evaluate.sql

--- bq_ml_evaluate.sql
+++ bq_ml_evaluate.sql
@@ -1,6 +1,6 @@
 // ==============================================================================
 // BigQuery ML Model Evaluation
-// Source: OZAT Engineering Blog (https://ozat.kz)
+// Source: OZAT Engineering Hub (https://ozat.kz)
 // GitHub: https://github.com/OZAT-kz/blog-codes/blob/main/bq_ml_evaluate.sql
 // ==============================================================================
 
