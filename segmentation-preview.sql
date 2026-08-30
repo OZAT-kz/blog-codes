178c32895719ab9fa647ebad25eaf29c743509c1
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:39:20 +0500

    Update header to OZAT Engineering Hub: segmentation-preview.sql

--- segmentation-preview.sql
+++ segmentation-preview.sql
@@ -1,9 +1,8 @@
-// ==============================================================================
-// segmentation-preview.sql
-// Source: OZAT Engineering Hub (https://ozat.kz)
-// GitHub: https://github.com/OZAT-kz/blog-codes/blob/main/segmentation-preview.sql
-// ==============================================================================
-
+-- ==============================================================================
+-- Paywall или AdSense? Как мы динамически скрывали рекламу от «китов» с помощью Firebase и Google Analytics
+-- Source: OZAT Engineering Hub (https://ozat.kz)
+-- GitHub: https://github.com/OZAT-kz/blog-codes/blob/main/segmentation-preview.sql
+-- ==============================================================================
 
 user_id | propensity_score | segment
 ------------------------------------
