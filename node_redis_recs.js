9f6bcf3524090d4982e56c822f22ef754b132ef7
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:35:29 +0500

    Update header to OZAT Engineering Hub: node_redis_recs.js

--- node_redis_recs.js
+++ node_redis_recs.js
@@ -1,6 +1,6 @@
 // ==============================================================================
 // Node.js Redis Recommendation Cache
-// Source: OZAT Engineering Blog (https://ozat.kz)
+// Source: OZAT Engineering Hub (https://ozat.kz)
 // GitHub: https://github.com/OZAT-kz/blog-codes/blob/main/node_redis_recs.js
 // ==============================================================================
 
