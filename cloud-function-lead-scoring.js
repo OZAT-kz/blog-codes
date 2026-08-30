be8ffa36787aa31f35c82f808acacffecc413eab
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:34:36 +0500

    Update header to OZAT Engineering Hub: cloud-function-lead-scoring.js

--- cloud-function-lead-scoring.js
+++ cloud-function-lead-scoring.js
@@ -1,6 +1,6 @@
 // ==============================================================================
 // cloud-function-lead-scoring.js
-// Source: OZAT Engineering Blog (https://ozat.kz)
+// Source: OZAT Engineering Hub (https://ozat.kz)
 // GitHub: https://github.com/OZAT-kz/blog-codes/blob/main/cloud-function-lead-scoring.js
 // ==============================================================================
 
