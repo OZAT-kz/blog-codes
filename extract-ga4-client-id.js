e1cdd580713f9d4aec509603eb4053286a1d60e3
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:35:07 +0500

    Update header to OZAT Engineering Hub: extract-ga4-client-id.js

--- extract-ga4-client-id.js
+++ extract-ga4-client-id.js
@@ -1,6 +1,6 @@
 // ==============================================================================
 // extract-ga4-client-id.js
-// Source: OZAT Engineering Blog (https://ozat.kz)
+// Source: OZAT Engineering Hub (https://ozat.kz)
 // GitHub: https://github.com/OZAT-kz/blog-codes/blob/main/extract-ga4-client-id.js
 // ==============================================================================
 
