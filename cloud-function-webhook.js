2d85e680e68014e63a6ec94b00ca529dc786e5f2
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:34:38 +0500

    Update header to OZAT Engineering Hub: cloud-function-webhook.js

--- cloud-function-webhook.js
+++ cloud-function-webhook.js
@@ -1,6 +1,6 @@
 // ==============================================================================
 // cloud-function-webhook.js
-// Source: OZAT Engineering Blog (https://ozat.kz)
+// Source: OZAT Engineering Hub (https://ozat.kz)
 // GitHub: https://github.com/OZAT-kz/blog-codes/blob/main/cloud-function-webhook.js
 // ==============================================================================
 
