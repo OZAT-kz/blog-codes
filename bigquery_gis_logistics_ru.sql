24a7c9ecada0d1faeec7f2bf98fff400dc8ddabc
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:34:26 +0500

    Update header to OZAT Engineering Hub: bigquery_gis_logistics_ru.sql

--- bigquery_gis_logistics_ru.sql
+++ bigquery_gis_logistics_ru.sql
@@ -1,6 +1,6 @@
 // ==============================================================================
 // bigquery_gis_logistics_ru.sql
-// Source: OZAT Engineering Blog (https://ozat.kz)
+// Source: OZAT Engineering Hub (https://ozat.kz)
 // GitHub: https://github.com/OZAT-kz/blog-codes/blob/main/bigquery_gis_logistics_ru.sql
 // ==============================================================================
 
