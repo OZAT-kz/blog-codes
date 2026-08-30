2b809fcc4738249232e204bf94b158a6bfeb4ce9
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:34:49 +0500

    Update header to OZAT Engineering Hub: dbt-user-features.sql

--- dbt-user-features.sql
+++ dbt-user-features.sql
@@ -1,6 +1,6 @@
 // ==============================================================================
 // dbt-user-features.sql
-// Source: OZAT Engineering Blog (https://ozat.kz)
+// Source: OZAT Engineering Hub (https://ozat.kz)
 // GitHub: https://github.com/OZAT-kz/blog-codes/blob/main/dbt-user-features.sql
 // ==============================================================================
 
