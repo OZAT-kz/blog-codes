b9459fc4c1fb1762b511d6139c2dc40d055ad56d
Rustam Sharafutdinov <staff@adkeeps.com>
2026-08-30 08:35:57 +0500

    Update header to OZAT Engineering Hub: useGeminiLiveCoffeeOrder_kz.tsx

--- useGeminiLiveCoffeeOrder_kz.tsx
+++ useGeminiLiveCoffeeOrder_kz.tsx
@@ -1,6 +1,6 @@
 // ==============================================================================
 // useGeminiLiveCoffeeOrder_kz.tsx
-// Source: OZAT Engineering Blog (https://ozat.kz)
+// Source: OZAT Engineering Hub (https://ozat.kz)
 // GitHub: https://github.com/OZAT-kz/blog-codes/blob/main/useGeminiLiveCoffeeOrder_kz.tsx
 // ==============================================================================
 
